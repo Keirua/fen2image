f8fd101aca64d68c6cb21913d7082eb83d8b9819
Clement CAMIN <c.camin.ext@meetic-corp.com>
2017-02-19 21:39:38 +0100

    Added drawing of icon

---
+++ README.md
@@ -0,0 +1,19 @@
+Purpose: 
+
+API endpoint : 
+
+https://en.lichess.org/study/{study_id}/{chapter_id}
+Example :
+
+GET /study/EMudCdxu/BXpApkse?_=1487534399267 HTTP/1.1
+Host: en.lichess.org
+User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0
+Accept: application/json, text/*, application/vnd.lichess.v1+json
+Accept-Language: en-US,en;q=0.5
+Accept-Encoding: gzip, deflate, br
+X-Requested-With: XMLHttpRequest
+Referer: https://en.lichess.org/study/EMudCdxu
+Connection: keep-alive
+
+Also requires Cookie
+
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
     "image"
     "image/color"
     "image/png"
+    "image/draw"
     "os"
 )
 
@@ -17,6 +18,7 @@ type Options struct {
 var g_Options Options;
 
 var fen = "8/8/8/4k3/5R2/8/8/3QK3 w - - 0 1";
+var ICON_SIZE = 32;
 
 func (options *Options) ParseCommandLineOptions() {
     flag.StringVar(&options.OutputFilename, "output", "out.png", "The output filename")
@@ -25,18 +27,43 @@ func (options *Options) ParseCommandLineOptions() {
     flag.Parse();
 }
 
+func loadIcon(inputFilename string) image.Image {
+    infile, err := os.Open(inputFilename)
+    if err != nil {
+        // replace this with real error handling
+        panic(err)
+    }
+    defer infile.Close()
+
+    // Decode will figure out what type of image is in the file on its own.
+    // We just have to be sure all the image packages we want are imported.
+    src, _, err := image.Decode(infile)
+    if err != nil {
+        // replace this with real error handling
+        panic(err)
+    }
+    return src;
+}
+
 func parseFen(fen string){
 
 }
 
+// 8ca2ad
+// dee3e6
+
 func main() {
     g_Options.ParseCommandLineOptions();
 
-    img := image.NewRGBA(image.Rect(0, 0, 100, 50))
+    img := image.NewRGBA(image.Rect(0, 0, 8*ICON_SIZE, 8*ICON_SIZE))
     img.Set(2, 3, color.RGBA{255, 0, 0, 255})
 
     f, _ := os.OpenFile("out.png", os.O_WRONLY|os.O_CREATE, 0600)
+    bishopIcon := loadIcon("icons/bbws.png")
     defer f.Close()
+
+    draw.Draw(img, img.Bounds(), bishopIcon, image.Point{0,0}, draw.Src);
+
     png.Encode(f, img)
 
     fmt.Println("Success !")
