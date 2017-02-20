f7c5df19f186c0056a6d728014086d883f705b50
Clement CAMIN <c.camin.ext@meetic-corp.com>
2017-02-20 13:53:23 +0100

    drawign a complete board

--- main.go
+++ main.go
@@ -20,6 +20,21 @@ var g_Options Options;
 var fen = "8/8/8/4k3/5R2/8/8/3QK3 w - - 0 1";
 var ICON_SIZE = 60;
 
+var board = [8][8]byte{  
+ {'r','n','b','q','k','b','n','r'},
+ {'p','p','p','p','p','p','p','p'},
+ {' ',' ',' ',' ',' ',' ',' ',' '},
+ {' ',' ',' ',' ',' ',' ',' ',' '},
+ {' ',' ',' ',' ',' ',' ',' ',' '},
+ {' ',' ',' ',' ',' ',' ',' ',' '},
+ {'P','P','P','P','P','P','P','P'},
+ {'R','N','B','Q','K','B','N','R'}};
+
+var validPieces = []byte{'r','n','b','q','k','p', 'R','N','B','Q','K','P'};
+
+// bishopIcon := loadIcon("icons/b60.png")
+var icons map[byte] *image.Image = make(map[byte] *image.Image);
+
 func (options *Options) ParseCommandLineOptions() {
     flag.StringVar(&options.OutputFilename, "output", "out.png", "The output filename")
     flag.StringVar(&options.Fen, "fen", "8/8/8/8/8/8/8/8 w - - 0 0", "The fen expression")
@@ -27,7 +42,7 @@ func (options *Options) ParseCommandLineOptions() {
     flag.Parse();
 }
 
-func loadIcon(inputFilename string) image.Image {
+func loadIcon(inputFilename string) *image.Image {
     infile, err := os.Open(inputFilename)
     if err != nil {
         // replace this with real error handling
@@ -42,11 +57,20 @@ func loadIcon(inputFilename string) image.Image {
         // replace this with real error handling
         panic(err)
     }
-    return src;
+    return &src;
 }
 
 func parseFen(fen string){
 
+}
+
+func contains(s []byte, e byte) bool {
+    for _, a := range s {
+        if a == e {
+            return true
+        }
+    }
+    return false
 }
 
 // 8ca2ad
@@ -60,12 +84,12 @@ func Rect(x1 int, y1, x2, y2 int, col color.RGBA, img *image.RGBA) {
     }
 }
 
-func DrawBoard(img *image.RGBA){
+func DrawBackground(img *image.RGBA){
     var whiteColor = color.RGBA{0, 128, 192, 255};
     var blackColor = color.RGBA{96, 96, 96, 255};
 
-    for x := 0; x<= 8; x++ {
-        for y := 0; y <= 8; y++ {
+    for x := 0; x< 8; x++ {
+        for y := 0; y < 8; y++ {
             color := blackColor;
             if (x+y)%2 == 0 {
                 color = whiteColor;
@@ -76,21 +100,45 @@ func DrawBoard(img *image.RGBA){
     }
 }
 
+func DrawBoard(board [8][8]byte, img *image.RGBA){
+    for x := 0; x< 8; x++ {
+        for y := 0; y < 8; y++ {
+            var piece = board[y][x];
+            fmt.Println(piece);
+            
+            var isValidPiece = piece != ' ' && contains(validPieces, piece);
+            var _, isLoaded = icons[piece];
+            if isValidPiece && isLoaded {
+                fmt.Println("Drawing " + string(piece));
+                draw.Draw(img, img.Bounds(), *(icons[piece]), image.Point{-x*ICON_SIZE,-y*ICON_SIZE}, draw.Over);
+            }
+        }
+    }
+    // draw.Draw(img, img.Bounds(), *(icons['R']), image.Point{6*ICON_SIZE,6*ICON_SIZE}, draw.Over);
+}
+
+func loadIcons(){
+    for _, s := range validPieces {
+        var iconFile = "icons/"+ string(s) +"60.png";
+        icons[s] = loadIcon(iconFile)
+    }
+}
+
 func main() {
     g_Options.ParseCommandLineOptions();
+    loadIcons();
 
     img := image.NewRGBA(image.Rect(0, 0, 8*ICON_SIZE, 8*ICON_SIZE))
     img.Set(2, 3, color.RGBA{255, 0, 0, 255})
 
     f, _ := os.OpenFile("out.png", os.O_WRONLY|os.O_CREATE, 0600)
-    bishopIcon := loadIcon("icons/b60.png")
+    
     defer f.Close()
 
-    DrawBoard(img);
-    draw.Draw(img, img.Bounds(), bishopIcon, image.Point{0,0}, draw.Over);
+    DrawBackground(img);
+    DrawBoard(board, img)
 
-    //Rect(32,100,100,130, color.RGBA{0, 128, 255, 255}, img);
-
+    // draw.Draw(img, img.Bounds(), *bishopIcon, image.Point{-60,-60}, draw.Over);
     png.Encode(f, img)
 
     fmt.Println("Success !")
